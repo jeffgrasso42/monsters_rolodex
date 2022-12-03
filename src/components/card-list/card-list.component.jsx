fd3ca0ffcdd883fce48f657f15706ec76167d746
JAG <jagrasso42@gmail.com>
2022-12-03 17:31:30 -0500

    add key prop for Card component in the CardList render method

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,10 +4,9 @@ import './card-list.styles.css'
 const CardList = ({monsters}) => {
     return (
       <div className='card-list'>
-        {monsters.map(monster => <Card monster={monster} />)}
+        {monsters.map(monster => <Card key={monster.id} monster={monster} />)}
       </div>
     );
-
 }
 
 export default CardList
