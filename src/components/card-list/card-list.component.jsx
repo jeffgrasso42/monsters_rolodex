80bbc704bb4b2fc2e120c2aa9cdf2f84755cb67d
JAG <jagrasso42@gmail.com>
2022-12-03 16:18:22 -0500

    add Card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react'
 
+import Card from '../card/card.component'
+
 import './card-list.styles.css'
 
 export default class CardList extends Component {
@@ -7,15 +9,8 @@ export default class CardList extends Component {
     const { monsters } = this.props;
     
     return (
-      <div className='card-list'>{monsters.map(monster => {
-        const {id, name, email} = monster;
-        return (
-        <div className='card-container' key={id}>
-          <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-          <h2>{name}</h2>
-          <p>{email}</p>
-          </div>
-        )})}
+      <div className='card-list'>
+        {monsters.map(monster => <Card monster={monster} />)}
       </div>
     );
   }
